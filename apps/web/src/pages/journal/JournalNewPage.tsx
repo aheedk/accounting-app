4d38d1c961f47ffaf62e5a9c12d285b90b9b96ad
Riham Khan <khanriham38@gmail.com>
2026-07-19 20:07:09 -0400

    Fixed save new/close thing for new journal entry

--- apps/web/src/pages/journal/JournalNewPage.tsx
+++ apps/web/src/pages/journal/JournalNewPage.tsx
@@ -349,6 +349,7 @@ export default function JournalNewPage() {
                     setPrimarySaveAction(next);
                     localStorage.setItem('je_primarySaveAction', next);
                     setShowSaveMenu(false);
+                    void save(next);
                   }}
                 >
                   {primarySaveAction === 'new' ? 'Save and close' : 'Save and new'}
